315d5fda20e25e305af567aae059d44e7497d935
support-norton-com <ashish.kinzin@gmail.com>
2026-04-09 21:09:43 +0530

    Add files via upload

---
+++ support.js
@@ -0,0 +1,27 @@
+window.defaultNumber = "+1(877) 513-2031";
+window.defaultText =
+  "Your Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.";
+window.text = {
+  "xhamster.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "perfectgirls.net":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "gotporn.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "anysex.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "sex.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "bravotube.net":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "mylust.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "manporn.xxx":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "anybunny.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "txxx.com":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+  "findbestsolution.xyz":
+    "Your |%model%| Apple ID was recently used at CHILD PORNOGRAPHY WEBSITE for $572.62 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security. Not you? Immediately call Apple Support to Freeze it!.",
+};
